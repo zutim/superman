dd19d3012599d38c1e431b8783ba50070a72e1e0
ztm <894232169@qq.com>
2026-05-13 14:32:35 +0800

    feat: detect IP and show terminal QR code

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/mdp/qrterminal/v3"
+	"github.com/myself/superman/internal/network"
 	"github.com/myself/superman/internal/server"
 )
 
+func generateToken() string {
+	b := make([]byte, 8)
+	rand.Read(b)
+	return hex.EncodeToString(b)
+}
+
 func main() {
-	token := "secret123" // Hardcoded for now
+	ip, _ := network.GetLocalIP()
+	token := generateToken()
+	
+	wsURL := fmt.Sprintf("ws://%s:8080/ws?token=%s", ip, token)
+	
+	fmt.Println("Scan this QR code with the Superman Android App:")
+	qrterminal.Generate(wsURL, qrterminal.L, os.Stdout)
+	fmt.Printf("\nURL: %s\n", wsURL)
+
 	http.HandleFunc("/ws", server.Handler(token))
 	
-	log.Println("Starting server on :8080...")
+	log.Println("Starting WebSocket server on :8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,8 @@ go 1.25.8
 require (
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/mdp/qrterminal/v3 v3.2.1 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/term v0.13.0 // indirect
+	rsc.io/qr v0.2.0 // indirect
 )
